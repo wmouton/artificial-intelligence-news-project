dec68e0fd0a11e590edde5105519564cfd4e0fbb
l33th <l33th@protonmail.ch>
2021-12-11 08:47:20 +0200

    added news card and news cards components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import './App.css';
+import NewsCards from './components/NewsCards/NewsCards';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const alanKey = API_KEY;
 
 function App() {
+	const [newsArticles, setNewsArticles] = useState([]);
 	useEffect(() => {
 		alanBtn({
 			key: alanKey,
 			onCommand: ({ command, articles }) => {
 				if (command === 'newHeadlines') {
-					console.log(articles);
+					setNewsArticles(articles);
 				}
 			},
 		});
@@ -22,6 +24,7 @@ function App() {
 			<h1 style={{ textAlign: 'center' }}>
 				Alan Artificial Intelligence Project
 			</h1>
+			<NewsCards articles={newsArticles} />
 		</div>
 	);
 }
