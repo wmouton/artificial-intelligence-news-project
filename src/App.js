8be214b6de144f0e15fe95cb7ea5361c9dbb2dc0
l33th <l33th@protonmail.ch>
2021-12-11 07:01:41 +0200

    alan button setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 import './App.css';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const alanKey = API_KEY;
+
 function App() {
+	useEffect(() => {
+		alanBtn({
+			key: alanKey,
+			onCommand: ({ command }) => {
+				if (command === 'testCommand') {
+					alert('this code was executed');
+				}
+			},
+		});
+	}, []);
 	return (
 		<div>
-			<p>Alan Artificial Intelligence Project</p>
+			<h1 style={{ textAlign: 'center' }}>
+				Alan Artificial Intelligence Project
+			</h1>
+			<p style={{ textAlign: 'center' }}>
+				Test Commands: <br />
+				<br /> 'What is this website?' <br /> 'How do I use this website?'
+			</p>
 		</div>
 	);
 }
