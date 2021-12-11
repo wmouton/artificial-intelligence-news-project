fa66dd0f1908b929ebe4224c3606279cb4d3403c
l33th <l33th@protonmail.ch>
2021-12-11 09:04:44 +0200

    render news cards

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Grid, Grow, Typography } from '@material-ui/core';
 import NewsCard from '../NewsCard/NewsCard';
 
 const NewsCards = ({ articles }) => {
 	return (
-		<div>
-			{articles.map((articles, i) => (
-				<NewsCard />
-			))}
-		</div>
+		<Grow in>
+			<Grid container alignItems='stretch' spacing={3}>
+				{articles.map((article, i) => (
+					<Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
+						<NewsCard article={article} i={i} />
+					</Grid>
+				))}
+			</Grid>
+		</Grow>
 	);
 };
 
