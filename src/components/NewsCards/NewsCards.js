069bc998f4acb8384ad6836e4bc1db069a5a4e15
l33th <l33th@protonmail.ch>
2021-12-11 09:10:15 +0200

    added useStyles hook

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Grid, Grow, Typography } from '@material-ui/core';
 import NewsCard from '../NewsCard/NewsCard';
+import useStyles from './styles';
 
 const NewsCards = ({ articles }) => {
+	const classes = useStyles();
 	return (
 		<Grow in>
-			<Grid container alignItems='stretch' spacing={3}>
+			<Grid
+				className={classes.container}
+				container
+				alignItems='stretch'
+				spacing={3}
+			>
 				{articles.map((article, i) => (
 					<Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
 						<NewsCard article={article} i={i} />
---
+++ src/components/NewsCards/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core';
+
+// const styles = makeStyles({
+export default makeStyles({
+	container: {
+		padding: '0 5%',
+		width: '100%',
+		margin: 0,
+	},
+});
