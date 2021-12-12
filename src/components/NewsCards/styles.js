25f5162d485615cd014e5c4750e45a97b8f42f62
l33th <l33th@protonmail.ch>
2021-12-12 13:39:40 +0200

    added styles to newscard - deleted newscards styles

--- src/components/NewsCards/styles.js
+++ src/components/NewsCards/styles.js
@@ -2,40 +2,5 @@ import { makeStyles } from "@material-ui/core";
 
 // const styles = makeStyles({
 export default makeStyles({
-  container: {
-    padding: "0 5%",
-    width: "100%",
-    margin: 0,
-  },
-  border: {
-    border: "solid",
-  },
-  fullHeightCard: {
-    height: "100%",
-  },
-  card: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    borderBottom: "10px solid white",
-  },
-  activeCard: {
-    borderBottom: "10px solid #22289a",
-  },
-  grid: {
-    display: "flex",
-  },
-  details: {
-    display: "flex",
-    justifyContent: "space-between",
-    margin: "20px",
-  },
-  title: {
-    padding: "0 16px",
-  },
-  cardActions: {
-    padding: "0 16px 8px 16px",
-    display: "flex",
-    justifyContent: "space-between",
-  },
+  
 });
