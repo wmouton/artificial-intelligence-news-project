2162d3b3128fe6c56d66cbca5facea8ab53803d7
l33th <l33th@protonmail.ch>
2021-12-14 14:44:41 +0200

    web app styling - errors in ai

--- src/App.js
+++ src/App.js
@@ -1,32 +1,72 @@
 import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
 import alanBtn from '@alan-ai/alan-sdk-web';
-import './App.css';
-import NewsCards from './components/NewsCards/NewsCards';
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+import logo from './images/logo.png';
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
 
-const alanKey = API_KEY;
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
-function App() {
-	const [newsArticles, setNewsArticles] = useState([]);
-	useEffect(() => {
-		alanBtn({
-			key: alanKey,
-			onCommand: ({ command, articles }) => {
-				if (command === 'newHeadlines') {
-					setNewsArticles(articles);
-				}
-			},
-		});
-	}, []);
-	return (
-		<div className='background'>
-			<h1 style={{ textAlign: 'center' }}>
-				Alan Artificial Intelligence News Reader
-			</h1>
-			<NewsCards articles={newsArticles} />
-		</div>
-	);
-}
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: '1866fc65ff31a5665bb7a16478a609112e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src="https://www.freeiconspng.com/uploads/artificial-intelligence-png-29.png" className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h4">
+            Created by
+            <a className={classes.link} href="https://www.linkedin.com/in/wmouton"> WMouton</a> -
+            <a className={classes.link} href="http://twitter.com/l33th_dev"> l33th</a>
+          </Typography>
+          <img className={classes.image} src={logo} height="50px" alt="wmouton logo" />
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
 export default App;
