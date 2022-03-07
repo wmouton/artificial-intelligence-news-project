754b5c310324ab6e74807fade0a23234d411a2b7
l33th <l33th@protonmail.ch>
2022-03-07 05:58:05 +0200

    updates

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -5,7 +5,7 @@ import NewsCard from './NewsCard/NewsCard';
 import useStyles from './styles.js';
 
 const infoCards = [
-  { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
+  { color: '#00838f', title: 'Latest News', text: 'Help' },
   { color: '#1565c0', title: 'News by Categories', info: 'Business, Science, Sports, Technology', text: 'Give me the latest Technology news' },
   { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones...', text: 'What\'s up with PlayStation 5' },
   { color: '#283593', title: 'News by Sources', info: 'CNN, BBC News, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
